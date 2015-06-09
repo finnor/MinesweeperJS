2f4952edfeeef9f4cc182f7e85934a4c9e003311
Adrian Flannery <aflanry@gmail.com>
2015-06-09 16:47:53 -0500

    Start of pattern based matching

--- js/solver.js
+++ js/solver.js
@@ -2,6 +2,7 @@ function Solver(game) {
 	this.game = game;
 	this.lastXScouted = 0;
 	this.lastYScouted = 0;
+	this.patterns = new Patterns();
 	this.createEdgesBoard = function (x, y) {
 		var edges = [];
 		for (var i=0; i<=x+1; i++) {
@@ -60,23 +61,26 @@ function Solver(game) {
 	
 	this.getMove = function () {
 		moveFound = false;
+		var move;
 		for(var i=1; i<=game.x; i++) {
 			for(var j=1; j<=game.y; j++) {
 				if (this.isEdge(i, j)) {
 					flagCount = game.getNeighboringFlagCount(i, j);
 					if (flagCount==game.board[i][j] && this.edges[i][j]> flagCount) {
-						var move = new Move(i, j, "X-Neighbors: Can click all neighbors");
+						move = new Move(i, j, "X-Neighbors: Can click all neighbors");
 						return move;
 					}
 					if (game.board[i][j]==this.edges[i][j] && this.edges[i][j]>flagCount) {
-						var move = new Move(i, j, "X-Neighbors: Can flag all neighbors");
+						move = new Move(i, j, "X-Neighbors: Can flag all neighbors");
 						return move;
 					}
+					move = patternMatching(i, j);
+					if (move!=null)
+						return move;
 				}
 			}
 		}
-		
-		return (new Move(0, 0, "No moves known"));
+			return (new Move(0, 0, "No moves known"));
 	}
 	
 	this.updateEdges = function(x, y, context) {
@@ -90,10 +94,50 @@ function Solver(game) {
 		}
 	}
 	
+	
 }
 
 function Move(x, y, message) {
 	this.x = x;
 	this.y = y;
 	this.message = message;
+}
+
+
+/*
+ * 
+ * 
+ * ? - visible
+ * - - Not a mine
+ */
+function Patterns() {
+	this.patterns = [
+	    [ 
+	    	[["?", "?", "?"],
+	    	 ["1", "2", "1"],
+	    	 ["-", "-", "-"]],
+	    	[["X", "+", "X"],
+	    	 [" ", " ", " "],
+	    	 [" ", " ", " "]],
+	  	],
+    ];
+	
+	this.rotate = function(pattern) {	
+		var newPattern = [];
+		for(var i=0; i<pattern.length; i++) {
+			newPattern[i] = [];
+			for (var j=0; j<pattern[i].length; j++) {
+				newPattern[i][j] = pattern[j][pattern.length-i-1]; 
+			}
+		}	
+		return newPattern;
+	}
+	
+	this.isMatch = function(x, y, inputPattern) {
+		for (var i=0; i<this.patterns.length; i++) {
+			for (var j=0; j<4; j++) {
+				
+			}
+		}
+	}
 }
