cf92b688ba81c66465a16697c11c078a32321593
Adrian Flannery <aflanry@gmail.com>
2015-06-11 11:05:31 -0500

    Allowing bigger patterns

--- js/solver.js
+++ js/solver.js
@@ -10,15 +10,18 @@ function Solver(game) {
 		for(var i=1; i<=game.x; i++) {
 			for(var j=1; j<=game.y; j++) {
 				if (game.isEdge(i, j)) {
-					/*flagCount = game.getNeighboringFlagCount(i, j);
+					flagCount = game.getNeighboringFlagCount(i, j);
+					
 					if (flagCount==game.board[i][j] && game.edges[i][j]> flagCount) {
 						move = new Move(i, j, "X-Neighbors: Can click all neighbors"); //Relies on flags being correct
+						move.allClick(game.flag, game.visible);
 						return move;
 					}
 					if (game.board[i][j]==game.edges[i][j] && game.edges[i][j]>flagCount) {
 						move = new Move(i, j, "X-Neighbors: Can flag all neighbors");
+						move.allMine(game.flag, game.visible);
 						return move;
-					}*/
+					}
 					move = this.patternMatching(i, j);
 					if (move!=null)
 						return move;
@@ -29,42 +32,68 @@ function Solver(game) {
 	}
 	
 	
-	this.getArea = function(x, y) {
+	this.getArea = function(x, y, length) {
 		var area = [];
-		for (var i=x-1, r=0; i<=x+1; i++, r++) {
+		var radius = Math.floor(length/2); 
+		for (var i=x-radius, r=0; i<=x+radius; i++, r++) {
 			area[r] = [];
-			for (var j=y-1, s=0; j<=y+1; j++, s++) {
-				if (i==0 || i==game.board.length-1 || j==0 || j==game.board[i].length-1) 
+			for (var j=y-radius, s=0; j<=y+radius; j++, s++) {
+				if (i<=0 || i>=game.board.length-1 || j<=0 || j>=game.board[i].length-1) 
 					area[r][s] = "E";
 				else if (game.visible[i][j]==false)
 					area[r][s] = "?";
 				else
 					area[r][s] = game.board[i][j];
 			}
-		}
-		
+		}	
 		return area;
 	}
 	
 	this.patternMatching = function(x, y) {
-		var inputPattern = this.getArea(x, y);
+		var inputPattern = [];
+		var inputPattern3 = this.getArea(x, y, 3);
+		var inputPattern5 = this.getArea(x, y, 5);
 		var key = game.board[x][y];
 		for (var i=0; i<this.patterns.patterns[key].length; i++) {
 			testPattern = this.patterns.patterns[key][i][0];
 			moveIfCorrect = this.patterns.patterns[key][i][1];
+			size = this.patterns.patterns[key][i][2];
+			
+			if (size==3)
+				inputPattern = inputPattern3;
+			else if (size==5)
+				inputPattern = inputPattern5;
 			for (var j=0; j<4; j++) {
 				if (this.patterns.match(inputPattern, testPattern)) {
-					move = new Move(x, y, "")
-					move.determineAction(moveIfCorrect)
-					console.debug("Match at:" + x + "|" + y);
-					console.debug(move);
-					return move;
+					move = new Move(x, y, "");
+					move.determineAction(moveIfCorrect);
+					if (this.testIfAlreadyPerformed(move)) {
+						testPattern = this.patterns.rotate(testPattern);
+						moveIfCorrect = this.patterns.rotate(moveIfCorrect);
+					} else {
+						return move;
+					}
 				} else {
 					testPattern = this.patterns.rotate(testPattern);
 					moveIfCorrect = this.patterns.rotate(moveIfCorrect);
 				}
 			}
 		}
+	}
+	
+	
+	this.testIfAlreadyPerformed = function(move) {
+		for(var i=0; i<move.canClick.length; i++) {
+			if(!(this.game.visible[move.canClick[i][0]][move.canClick[i][1]]))
+				return false;
+		}
+		
+		for(var i=0; i<move.canMine.length; i++) {
+			if(!(this.game.flag[move.canMine[i][0]][move.canMine[i][1]]))
+				return false;
+		}
+		
+		return true;
 	}
 	
 }
@@ -90,6 +119,30 @@ function Move(x, y, message) {
 			}
 		}
 	}
+	
+	this.allMine = function(flag, visible) {
+		for (var i=this.x-1; i<=this.x+1; i++) {
+			for (var j=this.y-1; j<=this.y+1; j++) {
+				if (i>0 && i<game.board.length && j>0 && j<game.board[i].length) { 
+					if (!flag[i][j] && !visible[i][j])
+						this.canMine.push([i, j]);
+				}
+			}
+		}
+	}
+	
+	this.allClick = function(flag, visible) {
+		for (var i=this.x-1; i<=this.x+1; i++) {
+			for (var j=this.y-1; j<=this.y+1; j++) {
+				if (i>0 && i<game.board.length && j>0 && j<game.board[i].length) { 
+					if (!flag[i][j] && !visible[i][j]) {
+						this.canClick.push([i, j]);
+					}
+				}
+			}
+		}
+	}
+		
 }
 
 
@@ -98,10 +151,25 @@ function Move(x, y, message) {
  * 
  * ? - not visible
  * - - Not a mine, but visible or out of bounds
+ *   - Anything
  */
 function Patterns() {
 	this.patterns = [];
-	this.patterns[1] = [];
+	this.patterns[1] = [
+        [
+	    	[[" ", " ", " ", " ", " "],
+	    	 ["-", "?", "?", "?", " "],
+	    	 ["-", "1", "1", " ", " "],
+	    	 ["-", "-", "-", " ", " "],
+	    	 ["-", "-", "-", "-", " "]],
+	    	[[" ", " ", " ", " ", " "],
+	    	 [" ", " ", " ", "+", " "],
+	    	 [" ", " ", " ", "+", " "],
+	    	 [" ", " ", " ", "+", " "],
+	    	 [" ", " ", " ", " ", " "]],
+	    	5,
+        ]
+    ];
 	this.patterns[2] = [
 	    [ 
 	    	[["?", "?", "?"],
@@ -110,9 +178,20 @@ function Patterns() {
 	    	[["X", "+", "X"],
 	    	 [" ", " ", " "],
 	    	 [" ", " ", " "]],
+	    	3,
 	  	],
     ];
-	this.patterns[3] = [];
+	this.patterns[3] = [
+        [
+	    	[["?", "?", "?"],
+	    	 ["1", "3", "?"],
+	    	 ["-", "-", "-"]],
+	    	[[" ", " ", "X"],
+	    	 [" ", " ", "X"],
+	    	 [" ", " ", " "]],
+	    	3,
+        ]
+    ];
 	this.patterns[4] = [];
 	this.patterns[5] = [];
 	
@@ -145,6 +224,8 @@ function Patterns() {
 			case "E":
 				result = this.testOutOfBounds(patternSymbol);
 				break;
+			case " ":
+				result = true;
 			default:
 				if (input==patternSymbol)
 					result = true;
@@ -180,6 +261,8 @@ function Patterns() {
 		
 		switch (patternSymbol) {
 			case "-":
+				return true;
+			case "E":
 				return true;
 			default:
 				return false;
